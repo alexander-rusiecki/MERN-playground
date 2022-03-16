59f6b1c7435108ff27bc49f1bdee6f26fea8eac8
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-03-16 14:53:29 +0100

    create Home component & use react-query + axios for api call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+import Home from './components/Home';
 import DogList from './components/DogList';
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <DogList />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="dogs" element={<DogList />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- client/src/components/DogList.tsx
+++ client/src/components/DogList.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 import { IDog } from '../models/DogInterface';
 
 function DogList() {
-  const [dogs, setDogs] = useState([]);
-  useEffect(() => {
-    const getDogs = async () => {
-      const response = await fetch('http://localhost:4000/api/dogs/');
-      setDogs(await response.json());
-    };
-    getDogs();
-  }, []);
+  const { isLoading, data } = useQuery('dogs', () => {
+    return axios.get('http://localhost:4000/api/dogs/');
+  });
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
   return (
-    <div>
+    <>
       <h1>Dogs:</h1>
-      {dogs
-        ? dogs.map((dog: IDog) => <h1 key={dog._id}>{dog.breed}</h1>)
-        : null}
-    </div>
+      {data?.data.map((dog: IDog) => (
+        <h1 key={dog._id}>{dog.breed}</h1>
+      ))}
+    </>
   );
 }
 
