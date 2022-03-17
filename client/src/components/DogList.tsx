bc06fc725ccf687214154b9eb916ac2b5d29044c
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-03-17 20:18:06 +0100

    add refetch on window focus

--- client/src/components/DogList.tsx
+++ client/src/components/DogList.tsx
@@ -3,13 +3,20 @@ import axios from 'axios';
 import { Dog } from 'components/Dog';
 import { IDog } from 'models/DogInterface';
 
+const fetchDogs = () => {
+  return axios.get<IDog[]>('http://localhost:4000/api/dogs/');
+};
+
 export const DogList = () => {
-  const { isLoading, data } = useQuery('dogs', () => {
-    return axios.get<IDog[]>('http://localhost:4000/api/dogs/');
+  const { isLoading, data, isError } = useQuery('dogs', fetchDogs, {
+    refetchOnWindowFocus: true,
   });
 
   if (isLoading) {
     return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return <h1>Error fetching data...</h1>;
   }
   return (
     <>
